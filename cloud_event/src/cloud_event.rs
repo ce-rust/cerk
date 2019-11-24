0db0c75f18eea80b862ea2c187df5a5219f0dd2a
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2019-11-24 10:19:06 +0100

    feat(cloud_events): update CloudEvent to comply with CloudEvents spec #5 - WIP

--- cloud_event/src/cloud_event.rs
+++ cloud_event/src/cloud_event.rs
@@ -1,5 +1,20 @@
+use chrono::naive::NaiveDateTime;
+
+#[derive(Clone, Debug)]
+pub enum Data {
+    String(String),
+    None,
+}
+
 #[derive(Clone, Debug)]
 pub struct CloudEvent {
     pub id: String,
+    pub event_type: String,
+    pub spec_version: String,
+    pub time: Option<NaiveDateTime>,
     pub source: String,
+    pub subject: String,
+    pub data_schema: String,
+    pub content_type: String,
+    pub data: Data,
 }
