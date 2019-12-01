6d8878a15adbe98a3418f3d96f22710693501a84
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2019-12-01 19:09:19 +0100

    feat: document: Config

--- cerk/src/kernel/config.rs
+++ cerk/src/kernel/config.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
+/// This object represents the configuration for a component.
+/// It could be defined recursively.
 pub enum Config {
     Null,
     Bool(bool),
-    // Number(Number),
     String(String),
     Array(Vec<Config>),
     Object(HashMap<String, Config>),
