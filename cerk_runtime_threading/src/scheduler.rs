a9b8d39b412ecc9aa91d7736d79bf240c90d0805
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2019-12-04 21:37:37 +0100

    feat: ThreadingScheduler as a function

--- cerk_runtime_threading/src/scheduler.rs
+++ cerk_runtime_threading/src/scheduler.rs
@@ -4,61 +4,44 @@ use cerk::runtime::channel::BoxedSender;
 use cerk::runtime::{InternalServerFn, InternalServerId};
 use std::thread;
 
-/// Struct for ThreadingScheduler
-///
-/// # open issues
-///
-/// * https://github.com/ce-rust/cerk/issues/24
-pub struct ThreadingScheduler {}
+/// A Scheduler implementation for CERK based on the `std::thread` model.
+/// `std::sync::mpsc` is used for the channels.
+/// The function that gets invoked in the `bootstrap` function in the start phase of CERK.
+pub fn threading_scheduler_start(start_options: StartOptions, start_kernel: KernelFn) {
+    info!("start threading scheduler");
 
-impl ThreadingScheduler {
-    fn new() -> Self {
-        ThreadingScheduler {}
-    }
-    /// A Scheduler implementation for CERK based on the `std::thread` model.
-    /// `std::sync::mpsc` is used for the channels.
-    /// The function that gets invoked in the `bootstrap` function in the start phase of CERK.
-    pub fn start(start_options: StartOptions, start_kernel: KernelFn) {
-        info!("start threading scheduler");
-        let mut scheduler = ThreadingScheduler::new();
-        scheduler.init(start_options, start_kernel);
-    }
+    let (sender_to_scheduler, receiver_from_kernel) = new_channel();
+    let (sender_to_kernel, receiver_from_scheduler) = new_channel();
 
-    fn init(&mut self, start_options: StartOptions, start_kernel: KernelFn) {
-        let (sender_to_scheduler, receiver_from_kernel) = new_channel();
-        let (sender_to_kernel, receiver_from_scheduler) = new_channel();
+    thread::spawn(move || {
+        start_kernel(start_options, receiver_from_scheduler, sender_to_scheduler);
+    });
 
-        thread::spawn(move || {
-            start_kernel(start_options, receiver_from_scheduler, sender_to_scheduler);
-        });
-
-        loop {
-            let event = receiver_from_kernel.receive();
-            match event {
-                BrokerEvent::ScheduleInternalServer(id, internal_server) => {
-                    self.schedule(id, internal_server, &sender_to_kernel)
-                }
-                _ => println!("Unknown event"),
+    loop {
+        let event = receiver_from_kernel.receive();
+        match event {
+            BrokerEvent::ScheduleInternalServer(id, internal_server) => {
+                schedule(id, internal_server, &sender_to_kernel)
             }
+            _ => warn!("Unknown event"),
         }
     }
+}
 
-    fn schedule(
-        &self,
-        id: InternalServerId,
-        internal_server_fn: InternalServerFn,
-        sender_to_kernel: &BoxedSender,
-    ) {
-        debug!("schedule {} thread", id);
-        let (sender_to_server, receiver_from_kernel) = new_channel();
-        let server_sender_to_kernel = sender_to_kernel.clone_boxed();
-        let new_server_id = id.clone();
-        thread::spawn(move || {
-            internal_server_fn(new_server_id, receiver_from_kernel, server_sender_to_kernel);
-        });
-        sender_to_kernel.send(BrokerEvent::InternalServerScheduled(
-            id.clone(),
-            sender_to_server,
-        ));
-    }
+fn schedule(
+    id: InternalServerId,
+    internal_server_fn: InternalServerFn,
+    sender_to_kernel: &BoxedSender,
+) {
+    debug!("schedule {} thread", id);
+    let (sender_to_server, receiver_from_kernel) = new_channel();
+    let server_sender_to_kernel = sender_to_kernel.clone_boxed();
+    let new_server_id = id.clone();
+    thread::spawn(move || {
+        internal_server_fn(new_server_id, receiver_from_kernel, server_sender_to_kernel);
+    });
+    sender_to_kernel.send(BrokerEvent::InternalServerScheduled(
+        id.clone(),
+        sender_to_server,
+    ));
 }
