00140707a3b70c622393ea91d7c44a7dd8637e58
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2020-11-21 13:38:41 +0100

    feat(kernel): add DeliveryGuarantee

---
+++ cerk/src/kernel/delivery_guarantees.rs
@@ -0,0 +1,62 @@
+use super::Config;
+use std::convert::TryFrom;
+
+/// Message delivery guarantees for the routing (defined per port channel)
+#[repr(u8)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum DeliveryGuarantee {
+    /// unspecified behaviour, the default
+    Unspecified = 0,
+    /// At Least Once the message should be received at the destination
+    AtLeastOnce = 2,
+}
+
+impl TryFrom<Config> for DeliveryGuarantee {
+    type Error = &'static str;
+    fn try_from(value: Config) -> Result<Self, Self::Error> {
+        if let Config::U8(number) = value {
+            match number {
+                0 => Ok(DeliveryGuarantee::Unspecified),
+                2 => Ok(DeliveryGuarantee::AtLeastOnce),
+                _ => Err("number out of range"),
+            }
+        } else {
+            Err("Config not of type U8")
+        }
+    }
+}
+
+impl From<DeliveryGuarantee> for Config {
+    fn from(value: DeliveryGuarantee) -> Self {
+        Config::U8(value as u8)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::error::Error;
+
+    #[test]
+    fn delivery_guarantee_to_config() {
+        let delivery_guarantee = DeliveryGuarantee::AtLeastOnce;
+        let config = Config::from(delivery_guarantee);
+        assert_eq!(config, Config::U8(DeliveryGuarantee::AtLeastOnce as u8));
+    }
+
+    #[test]
+    fn config_to_delivery_guarantee() -> Result<(), Box<dyn Error>> {
+        let config = Config::U8(DeliveryGuarantee::AtLeastOnce as u8);
+        let delivery_guarantee = DeliveryGuarantee::try_from(config)?;
+        assert_eq!(delivery_guarantee, DeliveryGuarantee::AtLeastOnce);
+        Ok(())
+    }
+
+    #[test]
+    fn failed_config_to_delivery_guarantee() -> Result<(), Box<dyn Error>> {
+        let config = Config::U8(99);
+        let delivery_guarantee = DeliveryGuarantee::try_from(config);
+        assert!(delivery_guarantee.is_err());
+        Ok(())
+    }
+}
--- cerk/src/kernel/mod.rs
+++ cerk/src/kernel/mod.rs
@@ -3,10 +3,12 @@
 mod bootstrap;
 mod broker_event;
 mod config;
+mod delivery_guarantees;
 mod kernel_start;
 mod start_options;
 
 pub use self::bootstrap::{bootstrap, KernelFn};
 pub use self::broker_event::BrokerEvent;
 pub use self::config::Config;
+pub use self::delivery_guarantees::DeliveryGuarantee;
 pub use self::start_options::StartOptions;
