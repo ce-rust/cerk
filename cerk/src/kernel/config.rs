f49c237dcaa50bb0ced8e9ca3a66bf85621f236c
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2020-11-24 21:21:40 +0100

    fix: formatting

--- cerk/src/kernel/config.rs
+++ cerk/src/kernel/config.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 /// This object represents the configuration for a component.
 /// It can be defined recursively.
