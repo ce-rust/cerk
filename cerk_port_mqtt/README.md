9f9eed0ab5d056424e293024e9bdd0fcf6dd4c7a
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2020-11-26 00:16:20 +0100

    chore: update readmes

--- cerk_port_mqtt/README.md
+++ cerk_port_mqtt/README.md
@@ -1,4 +1,4 @@
-# cerk_port_mqtt 0.2.0
+# cerk_port_mqtt 0.2.1
 
 [![Build status](https://badge.buildkite.com/4494e29d5f2c47e3fe998af46dff78a447800a76a68024e392.svg?branch=master)](https://buildkite.com/ce-rust/cerk)
 
--- check-readme.sh
+++ check-readme.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+param=$1
+
 function check {
+  if [ "$param" == "update" ]; then
+    cargo readme -r $1 > $1/README.md
+  else
     diff <(cargo readme  -r $1) $1/README.md || (echo 1>&2 "Please update the $1/README with "'`'"cargo readme -r $1 > $1/README.md"'`' && exit 1 )
+  fi
 }
 
 check cerk
