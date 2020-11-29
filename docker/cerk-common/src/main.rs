75e68b6f5b86718b8a11ccc2234ead78cb970cad
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2020-11-29 11:16:06 +0100

    feat(docker): add health check to docker-common

--- docker/cerk-common/src/main.rs
+++ docker/cerk-common/src/main.rs
@@ -4,12 +4,13 @@ extern crate cerk_loader_file;
 use cerk_config_loader_file::CONFIG_LOADER_FILE;
 use cerk_loader_file::{start, ComponentStartLinks};
 use cerk_port_amqp::PORT_AMQP;
-use cerk_port_mqtt::PORT_MQTT;
 use cerk_port_dummies::{PORT_PRINTER, PORT_SEQUENCE_GENERATOR};
-use cerk_router_rule_based::ROUTER_RULE_BASED;
-use cerk_router_broadcast::ROUTER_BROADCAST;
-use cerk_runtime_threading::THREADING_SCHEDULER;
+use cerk_port_health_check_http::PORT_HEALTH_CHECK_HTTP;
+use cerk_port_mqtt::PORT_MQTT;
 use cerk_port_unix_socket::{PORT_INPUT_UNIX_SOCKET, PORT_OUTPUT_UNIX_SOCKET};
+use cerk_router_broadcast::ROUTER_BROADCAST;
+use cerk_router_rule_based::ROUTER_RULE_BASED;
+use cerk_runtime_threading::THREADING_SCHEDULER;
 
 fn main() {
     env_logger::Builder::from_default_env().init();
@@ -24,7 +25,8 @@ fn main() {
             PORT_INPUT_UNIX_SOCKET,
             PORT_OUTPUT_UNIX_SOCKET,
             PORT_SEQUENCE_GENERATOR,
-            PORT_PRINTER
-         ],
+            PORT_PRINTER,
+            PORT_HEALTH_CHECK_HTTP
+        ],
     });
 }
