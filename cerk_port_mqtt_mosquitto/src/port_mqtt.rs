f0e1eb602fd080bb0dc20fa3f078ec7a2634b3a9
Linus Basig <linus@basig.me>
2020-12-05 12:19:32 +0100

    add blocking receiver

--- cerk_port_mqtt_mosquitto/src/port_mqtt.rs
+++ cerk_port_mqtt_mosquitto/src/port_mqtt.rs
@@ -1,11 +1,12 @@
 use mosquitto_client::{Mosquitto,Callbacks};
-use cerk::kernel::{BrokerEvent, CloudEventRoutingArgs, Config, IncomingCloudEvent};
+use cerk::kernel::{BrokerEvent, CloudEventRoutingArgs, Config, IncomingCloudEvent, DeliveryGuarantee};
 use cerk::runtime::channel::{BoxedReceiver, BoxedSender};
 use cerk::runtime::{InternalServerFn, InternalServerFnRefStatic, InternalServerId};
 use cloudevents::{AttributesReader, Event};
 use serde_json;
 use std::{thread,time};
 use anyhow::Result;
+use std::sync::mpsc::{channel,Sender};
 
 fn build_connection(id: &InternalServerId, config: Config) -> Result<Mosquitto> {
     match config {
@@ -49,16 +50,26 @@ fn build_connection(id: &InternalServerId, config: Config) -> Result<Mosquitto> 
     }
 }
 
-fn connect(client: Mosquitto) -> Result<()> {
+fn connect(id: InternalServerId, client: Mosquitto, sender_to_kernel: BoxedSender) -> Result<Sender<()>> {
+    let (sender, receiver) = channel();
     thread::spawn(move || {
         let mut callbacks = client.callbacks(Vec::<()>::new());
         callbacks.on_message(|data,msg| {
-            debug!("{:?} {:?}", data, msg);
+            let cloudevent: Event = serde_json::from_str(msg.text()).unwrap();
+            sender_to_kernel.send(BrokerEvent::IncomingCloudEvent(IncomingCloudEvent{
+                incoming_id:id.clone(),
+                routing_id: "abc".to_string(),
+                cloud_event: cloudevent,
+                args: CloudEventRoutingArgs {
+                    delivery_guarantee: DeliveryGuarantee::AtLeastOnce
+                }
+            }));
+            receiver.recv().unwrap();
         });
         client.loop_until_disconnect(200);
     });
 
-    return Ok(());
+    return Ok(sender);
 }
 
 fn send_cloud_event(
@@ -75,6 +86,7 @@ fn send_cloud_event(
 pub fn port_mqtt_start(id: InternalServerId, inbox: BoxedReceiver, sender_to_kernel: BoxedSender) {
     info!("start mqtt port with id {}", id);
     let mut client: Option<Mosquitto> = None;
+    let mut sender: Option<Sender<()>> = None;
 
     loop {
         match inbox.receive() {
@@ -85,13 +97,24 @@ pub fn port_mqtt_start(id: InternalServerId, inbox: BoxedReceiver, sender_to_ker
                 info!("{} received ConfigUpdated", &id);
                 client = Some(build_connection(&id, config).unwrap());
                 if let Some(ref client) = client {
-                    connect(client.clone());
+                    sender = Some(connect(id.clone(), client.clone(), sender_to_kernel.clone_boxed()).unwrap());
+                } else {
+                    // TODO
                 }
             }
             BrokerEvent::OutgoingCloudEvent(event) => {
                 debug!("{} cloudevent received", &id);
                 if let Some(ref client) = client {
                     send_cloud_event(&id, &event.cloud_event, &client).unwrap();
+                } else {
+                    // TODO
+                }
+            }
+            BrokerEvent::IncomingCloudEventProcessed(event_id, result) => {
+                if let Some(ref sender) = sender {
+                    sender.send(()).unwrap();
+                } else {
+                    // TODO
                 }
             }
             broker_event => warn!("event {} not implemented", broker_event),
