a9fc65a6aed12834359c89c2fd7c33c1d4f15ee9
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2021-01-13 11:09:07 +0100

    fix: check that really everything except the base is down

--- integration-tests/chaos-monkey/zz-automation/setup-base.sh
+++ integration-tests/chaos-monkey/zz-automation/setup-base.sh
@@ -14,6 +14,12 @@ kubectl apply -f ../continuous-run-config/ -f ../rabbitmq/ -f ../mosquitto/ -f .
 kubectl rollout status statefulsets.apps/rabbitmq --timeout=100s
 kubectl rollout status statefulsets.apps/mosquitto --timeout=100s
 
+# verify that only the new deployed components are here
+while [ $(kubectl get po | wc -l) -ne 4 ] || [ $(kubectl get po | grep "chaoskube" | wc -l) -ne 1 ] || [ $(kubectl get po | grep "rabbitmq" | wc -l) -ne 1  ] || [ $(kubectl get po | grep "mosquitto" | wc -l) -ne 1 ]; do
+    echo "wait for matching deployed..."
+    sleep 1
+done
+
 fuser -k 15672/tcp || true # kill process on port
 kubectl port-forward statefulset/rabbitmq 15672 || true &\
 while ! curl 127.0.0.1:15672 > /dev/null 2> /dev/null; do echo 'wait for port-forward...'; sleep 10; done
